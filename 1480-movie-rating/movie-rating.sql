69340cb999d4440cf2a7b33e5fa124d0e0f25931
Vakada Jeevan <vakadajeevan123456789@gmail.com>
2026-04-01 20:46:12 +0530

    Time: 1201 ms (72.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,22 @@
+# Find the user who has rated the greatest number of movies.
+(
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr ON u.user_id = mr.user_id
+    GROUP BY u.user_id
+    ORDER BY COUNT(mr.movie_id) DESC, u.name ASC
+    LIMIT 1
+)
+
+UNION ALL
+
+# Find the movie with the highest average rating in February 2020.
+(
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr ON m.movie_id = mr.movie_id
+    WHERE mr.created_at >= '2020-02-01' AND mr.created_at <= '2020-02-29'
+    GROUP BY m.movie_id
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
+    LIMIT 1
+);
